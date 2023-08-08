7c54d0829098229004dad4e22b5d9870006a8c19
stuzhou <73828319+yanyunwu@users.noreply.github.com>
2023-08-07 20:45:52 -0700

    chore: 新增ci自动发布

--- scripts/release.sh
+++
@@ -1,5 +0,0 @@
-git clean -fdx
-pnpm i
-pnpm build
-pnpm run changeset:version
-pnpm run changeset:publish
--- scripts/version.sh
+++ scripts/version.sh
@@ -1,6 +1,20 @@
 ORIGIN="origin"
 RELEASE_BRANCH=`git symbolic-ref --short -q HEAD`
 
+verify_release_branch() {
+  if [[ $RELEASE_BRANCH == main ]]
+  then
+    echo "当前处于 $RELEASE_BRANCH 分支"
+  else
+    echo "只能在 main 分支执行发布"
+  fi
+}
+
+verify_local_clean() {
+  git diff --quiet || echo "本地仓库有尚未提交的代码"
+  echo "本地仓库代码已提交！"
+}
+
 bump_version() {
   npx changeset version
   echo "更新 changelog 成功!"
@@ -13,6 +27,8 @@ push() {
   echo "已提交到 origin/$RELEASE_BRANCH"
 }
 
+verify_local_clean
+verify_release_branch
 bump_version
 push
 
