64d8b9d37ef23b6d353d1ec669d31b6030c8be04
yanyunwu <2480721346@qq.com>
2024-01-20 18:48:22 +0800

    docs: 修改文档

--- packages/others-ui/src/presets.ts
+++ packages/others-ui/src/presets.ts
@@ -1,7 +1,21 @@
-window.log = (...args: any[]) => {
-  if (__DEV__) {
-    window.console.log(...args)
+function getWindow(): Window | typeof globalThis {
+  if (typeof window !== 'undefined') {
+    return window
+  } else if (typeof global !== 'undefined') {
+    return global
+  } else if (typeof globalThis !== 'undefined') {
+    return globalThis
+  } else {
+    return null!
   }
 }
-window.warn = window.console.warn
-window.info = window.console.info
+
+const g = getWindow() as typeof window
+
+g.log = (...args: any[]) => {
+  if (__DEV__) {
+    g.console.log(...args)
+  }
+}
+g.warn = g.console.warn
+g.info = g.console.info
---
+++ scripts/build-docs.js
@@ -0,0 +1,22 @@
+import { copySync, removeSync } from 'fs-extra/esm'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { execa } from 'execa'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
+async function run() {
+  removeSync(resolve(__dirname, '../docs'))
+  removeSync(resolve(__dirname, '../packages/others-ui-docs/.vitepress/dist'))
+  await execa('pnpm', ['i'], { stdio: 'inherit' })
+  await execa('pnpm', ['build'], { stdio: 'inherit' })
+  await execa('pnpm', ['docs:build'], { stdio: 'inherit' })
+  copySync(
+    resolve(__dirname, '../packages/others-ui-docs/.vitepress/dist'),
+    resolve(__dirname, '../docs')
+  )
+}
+
+run()
