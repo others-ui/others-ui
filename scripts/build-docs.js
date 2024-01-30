1121354e7a74328bec0d3e81ac951544fc135106
stuzhou <73828319+yanyunwu@users.noreply.github.com>
2024-01-30 11:51:07 +0800

    chore(docs): 文档相关构建改为submodule (#20)
    * chore(docs): 文档相关构建改为submodule
    
    * chore: 修改文档构建
    
    * chore: 修改文档网站部署

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -8,15 +8,16 @@ const __dirname = dirname(__filename)
 
 
 async function run() {
-  removeSync(resolve(__dirname, '../docs'))
+  removeSync(resolve(__dirname, '../docs/docs'))
   removeSync(resolve(__dirname, '../packages/others-ui-docs/.vitepress/dist'))
   await execa('pnpm', ['i'], { stdio: 'inherit' })
   await execa('pnpm', ['build'], { stdio: 'inherit' })
   await execa('pnpm', ['docs:build'], { stdio: 'inherit' })
   copySync(
     resolve(__dirname, '../packages/others-ui-docs/.vitepress/dist'),
-    resolve(__dirname, '../docs')
+    resolve(__dirname, '../docs/docs')
   )
+  await execa('pnpm', ['docs:submodule-push'], { stdio: 'inherit' })
 }
 
 run()
