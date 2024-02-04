4cd1c5beeafcaae79a382d0e4c691a64194ed75a
stuzhou <73828319+yanyunwu@users.noreply.github.com>
2024-02-04 16:56:43 +0800

    feat: select组件完善 (#27)

--- packages/others-ui/src/components/select/select.ts
+++ packages/others-ui/src/components/select/select.ts
@@ -1,17 +1,30 @@
-import { css, html, unsafeCSS } from '@others-ui/common'
-import { property, state } from '@others-ui/common'
+import {
+  css,
+  html,
+  unsafeCSS,
+  PropertyValueMap,
+  TemplateResult,
+  property,
+  state,
+  BaseElement,
+  ifDefined,
+  ref,
+  createRef,
+  Ref,
+  classMap
+} from '@others-ui/common'
 import styles from './styles/select.scss'
-import { BaseElement } from '@others-ui/common'
-import { ifDefined } from '@others-ui/common'
+import { watch } from '../../utils/watch'
 
 export interface SelectOptions {
-  label?: string,
+  label?: string | TemplateResult<1>,
   value: string
 }
 
 export interface SelectProps {
   options: SelectOptions[]
   value?: string
+  placeholder?: string
 }
 
 export class Select extends BaseElement implements SelectProps {
@@ -25,28 +38,88 @@ export class Select extends BaseElement implements SelectProps {
   @property({type: String})
   public value?: string
 
+  @property({type: String})
+  public placeholder?: string
+
   @state()
   private _value?: string
 
   @state()
-  private label?: string
+  private label?: SelectOptions['label']
+
+  @state()
+  private active: boolean = false
+
+  private rootRef: Ref<HTMLDivElement> = createRef<HTMLDivElement>()
+
+  private get listHeight() {
+    return `${this.options.length * 30 + 10}px`
+  }
+
+  private setScopeListHeight(height: string) {
+    this.rootRef.value?.style.setProperty('--scope-list-height', height)
+  }
+
+  protected firstUpdated() {
+    this.setScopeListHeight(this.listHeight)
+
+    // 模拟active
+    let timer: NodeJS.Timer | null = null
+
+    const startActive = () => {
+      timer = setInterval(() => this.active = !this.active, 500)
+      let fn
+      document.addEventListener('click', fn = () => {
+        this.active = false
+        clearInterval(timer!)
+        timer = null
+        document.removeEventListener('click', fn!)
+      })
+    }
+
+    this.rootRef.value?.addEventListener('click', (e) => {
+      if (!timer) {
+        startActive()
+      }
+      e.stopPropagation()
+    })
+  }
 
   onItemClick(option: SelectOptions) {
     this.label = option.label ?? option.value
     this._value = option.value
     this.emit('change', this._value)
+    this.setScopeListHeight('0')
+
+    setTimeout(() => {
+      this.setScopeListHeight(this.listHeight)
+    }, 300)
   }
 
+  protected willUpdate(state: PropertyValueMap<SelectOptions>) {
+    watch(state, {
+      value: (val) => this._value = val
+    })
+  }
 
   render() {
     return html`
-    <div class="select">
-      <div class="select-active">
-        ${ifDefined(this.label)}
+    <div
+      class="select"
+      ${ref(this.rootRef)}
+    >
+      <div class="select-active-label" @click=${() => this.active = true}>
+        <div
+          class=${classMap({ 'placeholder-color': !this.label, active: this.active })}
+        >
+          ${ifDefined(this.label || this.placeholder)}
+        </div>
       </div>
-      <ul class="select-list">
-        ${this.options.map((option) => renderSelectItem.call(this, option))}
-      </ul>
+      <div class="select-list">
+        <ul class="select-list-box">
+          ${this.options.map((option) => renderSelectItem.call(this, option))}
+        </ul>
+      </div>
     </div>
     `
   }
