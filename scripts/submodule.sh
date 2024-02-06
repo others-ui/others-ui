91bda8c9960ec1da088deb196c4c07171ee09de6
stuzhou <73828319+yanyunwu@users.noreply.github.com>
2024-02-06 14:13:54 +0800

    docs(ci): docs update (#40)

--- scripts/submodule.sh
+++ scripts/submodule.sh
@@ -1,5 +1,6 @@
 ORIGIN="origin"
 RELEASE_BRANCH="main"
+HOST_RELEASE_BRANCH=`git symbolic-ref --short -q HEAD`
 
 cd ./docs
 git stash
@@ -9,5 +10,11 @@ git add .
 git commit -m "docs update"
 git push origin $RELEASE_BRANCH -f
 echo "文档已提交到 origin/$RELEASE_BRANCH"
+
+cd ..
+git add .
+git commit -m 'docs(ci): docs update'
+git push origin $HOST_RELEASE_BRANCH
+echo "主仓库已提交到 origin/$HOST_RELEASE_BRANCH"
 
 exit 0
